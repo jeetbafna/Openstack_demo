b3ac8ceb38e83cfea0993586931675840671bc2e
jeet bafna <jeetanil97@gmail.com>
2020-06-19 14:54:49 -0500

    Minor Changes

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ keystone.getToken('admin', 'a10ac0db07954235', function(error, token){
     console.log('A general token object has been retrived', token);
     global.token1=token.token;
     console.log(token1);
-    keystone.listProjects(token1, function(error, projects_array){
+    keystone.listProjects('admin', function(error, projects_array){
   if(error)
   {
     console.log('an error occured', error);
