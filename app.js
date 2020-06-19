e7cc37343d867ebef6c100460f9637687425755e
jeet bafna <jeetanil97@gmail.com>
2020-06-19 14:48:05 -0500

    Minor Changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var OSWrap = require('openstack-wrapper');
-var keystone = new OSWrap.Keystone('http://192.168.1.51:5000/v3');
+var keystone = new OSWrap.Keystone('http://192.168.1.51:5000');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
